25bfd53a997cb74e1d3ae6c94e7a44132fe45f01
Diego Nunes <diego.s.nune213@gmail.com>
2026-02-16 14:10:35 -0300

    Inserindo o nome do usuário ao invés do código

--- Diego/AD_PRATMOD/incluirNota/src/br/com/sankhya/acaoAtualizarFinanceiro/AtualizarFinanceiro.java
+++ Diego/AD_PRATMOD/incluirNota/src/br/com/sankhya/acaoAtualizarFinanceiro/AtualizarFinanceiro.java
@@ -8,19 +8,17 @@ import br.com.sankhya.jape.vo.DynamicVO;
 import br.com.sankhya.jape.wrapper.JapeFactory;
 import br.com.sankhya.jape.wrapper.JapeWrapper;
 import br.com.sankhya.modelcore.util.EntityFacadeFactory;
-import org.cuckoo.core.ScheduledAction;
-import org.cuckoo.core.ScheduledActionContext;
 
 import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 
 public class AtualizarFinanceiro implements AcaoRotinaJava {
 
     private final JapeWrapper movFinDAO = JapeFactory.dao("Financeiro");
     private final JapeWrapper TGFCABDAO = JapeFactory.dao("CabecalhoNota");
+    private final JapeWrapper usuDAO = JapeFactory.dao("Usuario");
 
     private Collection<DynamicVO> movFinVO;
 
@@ -32,39 +30,52 @@ public class AtualizarFinanceiro implements AcaoRotinaJava {
         try {
             setArrayNunotasCriadorNotas();
             setMovFinVO();
-            atualizaFinanceiro();
 
         } catch (Exception e) {
             throw new RuntimeException("Erro ao atualizar Financeiro: "+e);
         }
     }
 
-    private void setMovFinVO() throws Exception {
-        try{
-            this.movFinVO = this.movFinDAO.find("NUNOTA IN ?", this.nunotas.toArray());
-        }catch (Exception e){
-            throw new Exception("Erro ao buscar VO do financeiro: "+e);
-        }
+    private void setMovFinVO() {
+        this.nunotas.forEach(nunota -> {
+            try{
+                this.movFinVO = this.movFinDAO.find("NUNOTA = ?", nunota);
+                atualizaFinanceiro();
+            }catch (Exception e){
+                throw new RuntimeException("Erro ao buscar VOs do financeiro: "+e);
+            }
+        });
     }
 
     private void atualizaFinanceiro() {
         this.movFinVO.forEach( movFinVO -> {
             try {
-                String codUsuInc = retornaCodUsuInc(movFinVO);
+                String nomeUsuInc = retornaNomeUsuInc(movFinVO);
                 movFinDAO.prepareToUpdate(movFinVO)
-                        .set("HISTORICO", "Financeiros Gerados de Acordo com a Prática de Módulos Java - "+codUsuInc)
+                        .set("HISTORICO", "Financeiros Gerados de Acordo com o Módulo de Práticas Java - Nome do Usuário inclusão: "+nomeUsuInc)
                         .update();
+                System.out.println("Financeiro Atualiado: "+movFinVO);
             }catch (Exception e){
                 throw new RuntimeException("Erro ao atualizar financeiro: "+e);
             }
         });
     }
 
-    private String retornaCodUsuInc(DynamicVO movFinVO) throws Exception {
+    private String retornaNomeUsuInc(DynamicVO movFinVO) throws Exception {
         try {
-            return this.TGFCABDAO.findOne("NUNOTA = ? ", movFinVO.asBigDecimal("NUNOTA")).asBigDecimal("CODUSUINC").toString();
+            BigDecimal codUsuInc = retornaCodUsuInc(movFinVO);
+
+            return this.usuDAO.findOne("CODUSU = ?", codUsuInc).asString("NOMEUSU");
+        }catch (Exception e){
+            throw new Exception("Erro ao buscar nome do usuário inclusão: "+e);
+        }
+    }
+
+    private BigDecimal retornaCodUsuInc(DynamicVO movFinVO) throws Exception {
+        try {
+            return this.TGFCABDAO.findOne("NUNOTA = ? ", movFinVO.asBigDecimal("NUNOTA")).asBigDecimal("CODUSUINC");
         } catch (Exception e){
-            throw new Exception("Erro ao bucar usuário de inclusão: "+e);
+            throw new Exception("Erro ao bucar ID usuário de inclusão: "+e);
         }
     }
 
@@ -72,11 +83,11 @@ public class AtualizarFinanceiro implements AcaoRotinaJava {
         try {
             ResultSet nunota = retornaQueryCriadorNotas();
 
-
             while (nunota.next()) {
                 BigDecimal valorNunota = nunota.getBigDecimal("NUNOTA");
                 this.nunotas.add(valorNunota);
             }
+            System.out.println("setArrayNunotasCriadorNotas: "+ this.nunotas);
         }catch (Exception e){
             throw new Exception("Erro ao formar Array de NUNOTAS: "+e);
         }
